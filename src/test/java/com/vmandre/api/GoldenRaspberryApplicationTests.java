7d657bb86ee77979b41ba816b6503f2d6c766ebf
vmandre <vmandre@gmail.com>
2025-07-10 01:29:18 -0300

    :tada: init: Add initial commit

---
+++ src/test/java/com/vmandre/api/GoldenRaspberryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vmandre.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldenRaspberryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
